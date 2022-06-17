a3c5cc158c17840b457a8d432ea756b7c73ec614
AshishMalaviya2463 <ashishmalaviya2424@gmail.com>
2022-06-17 15:55:46 +0530

    n

--- src/container/LoginSignup.js
+++ src/container/LoginSignup.js
@@ -1,53 +1,108 @@
 import React, { useState } from 'react'
-import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
+import { FormGroup, Input, Label, Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import * as yup from 'yup';
+import { useFormik, Form, Formik } from 'formik';
 
 const LoginSignup = () => {
+
     const [regSignFor, setRegSignFor] = useState('register');
+
+    let schema = yup.object().shape({
+        name: yup.string().required('Please Enter Name.'),
+        email: yup.string().required('Please Enter Email.').email('Please Enter Valid Email.'),
+        password: yup.string().required('Please Enter Password.'),
+        createdOn: yup.date().default(function () {
+            return new Date();
+        }),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            name: '',
+            password: '',
+        },
+        validationSchema: schema,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    let schemaOfLogIn = yup.object().shape({
+        email: yup.string().required('Please Enter Email.').email('Please Enter Valid Email.'),
+        password: yup.string().required('Please Enter Password.'),
+        createdOn: yup.date().default(function () {
+            return new Date();
+        }),
+    });
+
+    const formikOfLogIn = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: schemaOfLogIn,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+
     return (
         <>
             {
                 regSignFor === 'register' ? <div className="container my-5">
                     <h2 className='mb-4'>Register</h2>
-                    <Form>
-                        <FormGroup>
-                            <Label for="exampleText">
-                                Name
-                            </Label>
-                            <Input
-                                id="exampleText"
-                                name="name"
-                                placeholder="Enter Your Name Here"
-                                type="text"
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="exampleEmail">
-                                Email
-                            </Label>
-                            <Input
-                                id="exampleEmail"
-                                name="email"
-                                placeholder="Enter Your E-mail Here"
-                                type="email"
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="examplePassword">
-                                Password
-                            </Label>
-                            <Input
-                                id="examplePassword"
-                                name="password"
-                                placeholder="Enter Your Password"
-                                type="password"
-                            />
-                        </FormGroup>
+                    <Formik values={formik}>
+                        <Form onSubmit={formik.handleSubmit}>
+                            <FormGroup>
+                                <Label for="exampleText">
+                                    Name
+                                </Label>
+                                <Input
+                                    id="exampleText"
+                                    name="name"
+                                    placeholder="Enter Your Name Here"
+                                    type="text"
+                                    onChange={formik.handleChange}
+                                />
+                                <p style={{ color: 'red' }}>{formik.errors.name}</p>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="exampleEmail">
+                                    Email
+                                </Label>
+                                <Input
+                                    id="exampleEmail"
+                                    name="email"
+                                    placeholder="Enter Your E-mail Here"
+                                    type="email"
+                                    onChange={formik.handleChange}
+                                />
+                                <p style={{ color: 'red' }}>{formik.errors.email}</p>
 
-                        <Button color="success">
-                            Submit
-                        </Button>
-                    </Form>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="examplePassword">
+                                    Password
+                                </Label>
+                                <Input
+                                    id="examplePassword"
+                                    name="password"
+                                    placeholder="Enter Your Password"
+                                    type="password"
+                                    onChange={formik.handleChange}
+                                />
+                                <p style={{ color: 'red' }}>{formik.errors.password}</p>
+
+                            </FormGroup>
+
+                            <Button color="success" type='submit'>
+                                Submit
+                            </Button>
+                        </Form>
+                    </Formik>
                     <div className='mt-3'>
                         <p>
                             If You Have An Account {' '}{' '}
@@ -57,34 +112,40 @@ const LoginSignup = () => {
                     </div>
                 </div> : <div className="container my-5">
                     <h2 className='mb-4'>Sign In</h2>
-                    <Form>
-                        <FormGroup>
-                            <Label for="exampleEmail">
-                                Email
-                            </Label>
-                            <Input
-                                id="exampleEmail"
-                                name="email"
-                                placeholder="Enter Your E-mail Here"
-                                type="email"
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="examplePassword">
-                                Password
-                            </Label>
-                            <Input
-                                id="examplePassword"
-                                name="password"
-                                placeholder="Enter Your Password"
-                                type="password"
-                            />
-                        </FormGroup>
+                    <Formik values={formikOfLogIn}>
+                        <Form onSubmit={formikOfLogIn.handleSubmit}>
+                            <FormGroup>
+                                <Label for="exampleEmail">
+                                    Email
+                                </Label>
+                                <Input
+                                    id="exampleEmail"
+                                    name="email"
+                                    placeholder="Enter Your E-mail Here"
+                                    type="email"
+                                    onChange={formikOfLogIn.handleChange}
+                                />
+                                <p style={{ color: 'red' }}>{formikOfLogIn.errors.email}</p>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="examplePassword">
+                                    Password
+                                </Label>
+                                <Input
+                                    id="examplePassword"
+                                    name="password"
+                                    placeholder="Enter Your Password"
+                                    type="password"
+                                    onChange={formikOfLogIn.handleChange}
+                                />
+                                <p style={{ color: 'red' }}>{formikOfLogIn.errors.password}</p>
+                            </FormGroup>
 
-                        <Button color="success">
-                            Submit
-                        </Button>
-                    </Form>
+                            <Button color="success" type='submit'>
+                                Submit
+                            </Button>
+                        </Form>
+                    </Formik>
                     <div className='mt-3'>
                         <NavLink
                             to="/forgotpassword"
@@ -97,7 +158,7 @@ const LoginSignup = () => {
                             If You Are A New User.
                             <br />
                             Create An Account {' '}{' '}
-                            <a onClick={()=>setRegSignFor('register')}
+                            <a onClick={() => setRegSignFor('register')}
                                 href="js:"
                             >
                                 Click Here
